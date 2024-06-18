8fd1426a3be1eab7b9c9c15e8e69b52bed2a347e
Victor Lawrence <viclawrence99@gmail.com>
2024-06-18 21:45:14 +0100

    added swiper

--- src/pages/Home/components/Testimonials.tsx
+++ src/pages/Home/components/Testimonials.tsx
@@ -1,4 +1,5 @@
 // import { useState } from "react";
+import { Swiper, SwiperSlide } from "swiper/react";
 
 const Testimonials = () => {
   const testimonies = [
@@ -19,17 +20,30 @@ const Testimonials = () => {
   //   const [currentIndex, setCurrentIndex] = useState(0);
 
   return (
-    <div>
-      <h2>Testimonials</h2>
-      <div className="flex gap-10">
-        {testimonies.map(({ name, desc }, index) => (
-          <div key={index}>
-            <p>{desc}</p>
-            <h3>{name}</h3>
-          </div>
-        ))}
+    <>
+      <Swiper
+        spaceBetween={50}
+        slidesPerView={3}
+        onSlideChange={() => console.log("slide change")}
+        onSwiper={(swiper) => console.log(swiper)}
+      >
+        <SwiperSlide>Slide 1</SwiperSlide>
+        <SwiperSlide>Slide 2</SwiperSlide>
+        <SwiperSlide>Slide 3</SwiperSlide>
+        <SwiperSlide>Slide 4</SwiperSlide>
+      </Swiper>
+      <div>
+        <h2>Testimonials</h2>
+        <div className="flex gap-10">
+          {testimonies.map(({ name, desc }, index) => (
+            <div key={index}>
+              <p>{desc}</p>
+              <h3>{name}</h3>
+            </div>
+          ))}
+        </div>
       </div>
-    </div>
+    </>
   );
 };
 
