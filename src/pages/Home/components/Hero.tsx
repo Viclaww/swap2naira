8655c87445b7783205de888815f5317b81dc3db0
Victor Lawrence <viclawrence99@gmail.com>
2024-06-20 01:06:44 +0100

    responsive

--- src/pages/Home/components/Hero.tsx
+++ src/pages/Home/components/Hero.tsx
@@ -3,12 +3,12 @@ const Hero = () => {
     <section className="flex justify-center md:flex-row flex-col-reverse bg-gradient-to-b from-blueZ/10 to-white items-center overflow-hidden px-8 z-0 md:px-10 w-[100vw] h-[82vh] font-medium">
       <div className="bg-blueX w-[350px] -z-20 rounded-full blur-[100px] opacity-30 top-8 -right-32  h-[350px] absolute"></div>
       <div className="bg-blueX w-[350px] contrast-75 z-20 rounded-full blur-[100px] opacity-30 top-[50vh] -left-16 h-[350px] absolute"></div>
-      <div className="md:w-[50%] flex flex-col gap-4 relative z-40">
-        <h2 className="md:text-7xl font-bold text-center text-3xl  md:font-semibold">
+      <div className="md:w-[50%] flex md:text-left text-center flex-col gap-4 relative z-40">
+        <h2 className="md:text-7xl font-bold  text-3xl  md:font-semibold">
           Sell your <span className="text-blueX">Gift Cards</span> at the best
           rates instantly
         </h2>
-        <p className="font-medium text-center text-lg">
+        <p className="font-medium t text-lg">
           Got gift cards? Sell them for cash Now. Enjoy a quick, secure, and
           hassle-free process. Turn your gift cards into money you can use
           anywhere, anytime!
--- src/pages/Home/components/Testimonials.tsx
+++ src/pages/Home/components/Testimonials.tsx
@@ -5,47 +5,6 @@ import "swiper/css/navigation";
 import { Swiper, SwiperSlide } from "swiper/react";
 import { Autoplay } from "swiper/modules";
 
-// <!-- Initialize Swiper -->
-// var swiper = new Swiper(".mySwiper", {
-//   // Optional parameters
-//   // slidesPerView: 5,
-//   spaceBetween: 0,
-//   centeredSlides: true,
-//   speed: 1500,
-//   autoplay: {
-//     delay: 0,
-//   },
-//   loop: true,
-//   slidesPerView: 'auto',
-//   allowTouchMove: false,
-//   disableOnInteraction: true,
-// If we need pagination
-// pagination: {
-//   el: ".swiper-pagination",
-//   clickable: true,
-// },
-// Navigation arrows
-// navigation: {
-//   nextEl: '.swiper-button-next',
-//   prevEl: '.swiper-button-prev',
-// },
-// Breakpoints
-// breakpoints: {
-//   640: {
-//     slidesPerView: 2,
-//     spaceBetween: 20,
-//   },
-//   768: {
-//     slidesPerView: 4,
-//     spaceBetween: 40,
-//   },
-//   1024: {
-//     slidesPerView: 5,
-//     spaceBetween: 50,
-//   },
-// },
-// });
-
 const Testimonials = () => {
   const testimonies = [
     {
@@ -78,30 +37,43 @@ const Testimonials = () => {
     },
   ];
 
-  //   const [currentIndex, setCurrentIndex] = useState(0);
-
   return (
     <>
-      {/* <div>
+      <div className="text-white">
+        <h2 className="text-5xl font-bold text-center my-10 text-blueBlack">
+          Testimonials
+        </h2>
         <Swiper
           spaceBetween={30}
-          centeredSlides={true}
-          slidesPerView={3}
+          centeredSlides={false}
+          slidesPerView={"auto"}
+          allowTouchMove={false}
+          speed={2500}
           autoplay={{
-            delay: 2500,
-            disableOnInteraction: false,
+            delay: 0,
           }}
-          pagination={{
-            clickable: true,
-          }}
-          navigation={true}
-          modules={[Autoplay, Pagination, Navigation]}
+          loop={true}
+          modules={[Autoplay]}
           className="mySwiper"
+          breakpoints={{
+            640: {
+              slidesPerView: 2,
+              spaceBetween: 20,
+            },
+            768: {
+              slidesPerView: 4,
+              spaceBetween: 40,
+            },
+            1024: {
+              slidesPerView: 3.3,
+              spaceBetween: 50,
+            },
+          }}
         >
           <div className="flex px-5 gap-10">
             {testimonies.map(({ name, desc }, index) => (
-              <SwiperSlide>
-                <div className="bg-blueBlack p-4" key={index}>
+              <SwiperSlide key={index}>
+                <div className="bg-blueBlack p-4">
                   <span className="text-8xl font-mono">"</span>
                   <p>{desc}</p>
                   <h3>{name}</h3>
@@ -110,53 +82,6 @@ const Testimonials = () => {
             ))}
           </div>
         </Swiper>
-      </div> */}
-
-      <div className="text-white">
-        <h2 className="text-5xl font-bold text-center my-10 text-blueBlack">
-          Testimonials
-        </h2>
-        <div>
-          <Swiper
-            spaceBetween={30}
-            centeredSlides={false}
-            slidesPerView={"auto"}
-            allowTouchMove={false}
-            speed={2500}
-            autoplay={{
-              delay: 0,
-            }}
-            loop={true}
-            modules={[Autoplay]}
-            className="mySwiper"
-            breakpoints={{
-              640: {
-                slidesPerView: 2,
-                spaceBetween: 20,
-              },
-              768: {
-                slidesPerView: 4,
-                spaceBetween: 40,
-              },
-              1024: {
-                slidesPerView: 3.3,
-                spaceBetween: 50,
-              },
-            }}
-          >
-            <div className="flex px-5 gap-10">
-              {testimonies.map(({ name, desc }, index) => (
-                <SwiperSlide>
-                  <div className="bg-blueBlack p-4" key={index}>
-                    <span className="text-8xl font-mono">"</span>
-                    <p>{desc}</p>
-                    <h3>{name}</h3>
-                  </div>
-                </SwiperSlide>
-              ))}
-            </div>
-          </Swiper>
-        </div>
       </div>
     </>
   );
