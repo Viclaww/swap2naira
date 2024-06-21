b37c22f25246c5d0fb3cb9657d7f93af89090f3c
Victor Lawrence <viclawrence99@gmail.com>
2024-06-21 17:22:16 +0100

    Added

--- src/pages/aurh/Layout.tsx
+++ src/pages/aurh/Layout.tsx
@@ -7,7 +7,7 @@ export default function AuthLayout({
     <div className="flex w-full h-screen justify-center items-center">
       <div className="bg-blueX/30 gap-4 flex px-4 rounded-lg py-6 flex-col w-2/5 items-center">
         <img src="/images/S2N.png" className="w-48" alt="logo" />
-        <form className="flex flex-col w-full gap-3">{children}</form>
+        <form className="auth-form flex flex-col w-full gap-3">{children}</form>
       </div>
     </div>
   );
