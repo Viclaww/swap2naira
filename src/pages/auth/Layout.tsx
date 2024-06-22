fb809c2c7ef8bb6b991f13ed3d61b505fca5e2bb
Victor Lawrence <viclawrence99@gmail.com>
2024-06-22 09:27:36 +0100

    changed color

--- src/pages/auth/Layout.tsx
+++ src/pages/auth/Layout.tsx
@@ -1,3 +1,5 @@
+import { Link } from "react-router-dom";
+
 export default function AuthLayout({
   children,
 }: {
@@ -5,14 +7,17 @@ export default function AuthLayout({
 }) {
   return (
     <div className="flex w-full h-screen justify-center items-center">
-      <div className="absolute -z-20 grid grid-cols-2 w-screen h-screen">
+      <div className="absolute bg-blueX/60 -z-20 grid grid-cols-2 w-screen h-screen">
         <img src="/images/authbg1.png" alt="" />
         <img src="/images/authbg2.png" alt="" />
         <img src="/images/authbg1.png" alt="" />
         <img src="/images/authbg2.png" alt="" />
       </div>
-      <div className="bg-blueX   gap-4 flex px-4 rounded-lg py-6 flex-col w-2/5 items-center">
-        <img src="/images/S2N.png" className="w-48 bg-white p-1" alt="logo" />
+      <div className="bg-white   gap-4 flex px-4 rounded-lg py-6 flex-col w-2/5 items-center">
+        <Link to="/">
+          <img src="/images/S2N.png" className="w-48 bg-white p-1" alt="logo" />
+        </Link>
+
         <form className="auth-form px-12 flex flex-col w-full gap-3">
           {children}
         </form>
