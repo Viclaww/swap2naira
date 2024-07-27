2a911bc9cc6b3f044103e9a4e1b386cb48586216
Victor Lawrence <viclawrence99@gmail.com>
2024-07-27 09:27:44 +0100

    Stupid

--- src/lib/api/generalApi.tsx
+++ src/lib/api/generalApi.tsx
@@ -104,7 +104,6 @@ export const {
   useVerifyForgotPassMutation,
   useGetNotificationsQuery,
   useResendForgotPasswordOTPMutation,
-  useLazyResendVerifyEmailQuery,
   useLazyLogoutUserQuery,
   useChangePasswordMutation,
   useLazyResendVerifyEmailQuery,
